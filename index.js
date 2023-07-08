ee74c0bb712ce37852d68095dc5fdfba77d38f28
Amit Sarker <amitsarkerstock@gmail.com>
2023-07-08 11:05:36 +0600

    loaded all doctors data

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 2020;
+require("dotenv").config();
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+};
+app.use(cors(corsConfig));
+app.use(express.json());
+
+// Mongo DB
+const uri = `mongodb+srv://${process.env.DB_USERS}:${process.env.DB_PASS}@cluster0.pq4nrld.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    const doctorsCollection = client.db("greenCare").collection("doctors");
+    app.get("/doctors", async (req, res) => {
+      const result = await doctorsCollection.find().toArray();
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Green Care is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Grean care in running on port ${port}`);
+});
