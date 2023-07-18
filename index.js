bcec8f7f11029828a22a1689053caabaac2dd087
Amit Sarker <amitsarkerstock@gmail.com>
2023-07-18 12:10:54 +0600

    date and time pick post req added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 2020;
 require("dotenv").config();
 const corsConfig = {
@@ -38,6 +38,18 @@ async function run() {
     app.get("/doctors", async (req, res) => {
       const result = await doctorsCollection.find().toArray();
       res.send(result);
+    });
+
+    app.get("/doctors/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await doctorsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/pickSlot", async (req, res) => {
+      const slotDetails = req.body;
+      console.log(slotDetails);
     });
   } finally {
     // Ensures that the client will close when you finish/error
